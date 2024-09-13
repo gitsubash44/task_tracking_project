cc2ee96e640195cc337e96c0161b1aec5c0690e0
Suabsh Dhami <110603740+gitsubash44@users.noreply.github.com>
2024-09-13 18:53:15 +0545

    worked

--- task_tracking_app/models.py
+++ task_tracking_app/models.py
@@ -2,37 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    PRIORITY_CHOICES = [
-        ('low', 'Low'),
-        ('medium', 'Medium'),
-        ('high', 'High'),
-    ]
-
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('in_progress', 'In Progress'),
-        ('completed', 'Completed'),
-    ]
 
     title = models.CharField(max_length=255)
     description = models.TextField()
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
     due_date = models.DateField()
-    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=10)
+    status = models.CharField(max_length=15)
 
     def __str__(self):
         return self.title
 
-
-# class Comment(models.Model):
-#     task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     content = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return f'Comment by {self.user.username} on {self.task.title}'
 
 
 class TaskNew(models.Model):
